7306126cb47da3a42288d48aac874ec33bb2053e
yuyuchien <80609830+yuyuchien@users.noreply.github.com>
2021-03-21 01:13:40 +0800

    Add files via upload

---
+++ homework01.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+for i in range(5,0,-1):
+    for j in range(0,i):
+       print('*',end='')
+    if i==2 :
+        break
+    print()
+for a in range(0,6):
+    for b in range(0,a):
+        print('*',end='')
+    print()
